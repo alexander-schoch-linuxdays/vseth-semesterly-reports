3c6cbe10cad08cb3dc6dafd1a3f52e1c38e9a7b8
Florian Moser <git@famoser.ch>
2020-03-13 12:00:27 +0100

    full namespace import

--- src/Respository/OrganisationRepository.php
+++ src/Respository/OrganisationRepository.php
@@ -9,12 +9,20 @@
  * file that was distributed with this source code.
  */
 
+/*
+ * This file is part of the vseth-semesterly-reports project.
+ *
+ * (c) Florian Moser <git@famoser.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\Repository;
 
 use App\Entity\Organisation;
-use Doctrine\ORM\EntityRepository;
 
-class OrganisationRepository extends EntityRepository
+class OrganisationRepository extends \Doctrine\ORM\EntityRepository
 {
     /**
      * @return Organisation[]
